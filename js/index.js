f02205baecf4cd7f09429b1782a20bde5316332b
Marsel Gabbasov <marsel13_98@mail.ru>
2022-01-14 16:55:26 +0300

    markup

--- js/index.js
+++ js/index.js
@@ -19,12 +19,10 @@ function sliderMain() {
 
 
 $(document).ready(function() {
-    cartCalc()
     sliderMain()
 
+    $(".select").niceSelect()
     $('input[type="tel"]').mask('+7 (999) 999-9999', { placeholder: '+7 (   )    -    ' });
-
-
 
     $(".nav__item").hover(onIn, onOut);
     $(".nav__item-show").click(function() {
@@ -65,102 +63,65 @@ $(document).ready(function() {
         }
     })
 
+    $(".service-item__all").click(function() {
+        $(this).hide()
+        $(".service-item__card").removeClass("service-item__card--hidden")
+    })
 
+    $(".tarifs__item").click(function() {
+        if (window.innerWidth < 992) {
+            $(this).toggleClass("tarifs__item--active")
+        }
+    })
 
+    $(".select-tarif__show").click(function() {
+        if (!$(this).hasClass("select-tarif__show--active")) {
+            $(this).addClass("select-tarif__show--active")
+        } else {
+            $(this).removeClass("select-tarif__show--active")
+        }
+    })
+
+    $(".select-tarif__hidden-item").click(function() {
+        let path = $(this).attr("data-tarif-show")
+        $(".select-tarif__show-item").removeClass("select-tarif__show-item--active")
+        $(`.select-tarif__show-item[data-tarif-show="${path}"]`).addClass("select-tarif__show-item--active")
+        $(".select-tarif__hidden-item").removeClass("select-tarif__hidden-item--active")
+        $(`.select-tarif__hidden-item[data-tarif-show="${path}"]`).addClass("select-tarif__hidden-item--active")
+        $(".select-tarif__show").removeClass("select-tarif__show--active")
+    })
 
 
 
     $(document).mouseup(function(e) { // событие клика по веб-документу
-        var div = $(".header__catalog-hidden");
+        var div = $(".select-tarif");
         if (!div.is(e.target) // если клик был не по нашему блоку
             &&
             div.has(e.target).length === 0) { // и не по его дочерним элементам
-            div.parents(".header__catalog").removeClass("header__catalog--active")
-                // $(".header__search-select-show").removeClass("header__search-select-show--active")
-        }
-    });
-
-
-
-
-    $(".collection__tab").click(function() {
-        let path = $(this).attr("data-tabs-path")
-        console.log(path)
-        $(".collection__tab").removeClass("collection__title--active")
-        $(`.collection__tab[data-tabs-path="${path}"]`).addClass("collection__title--active")
-        $(".collection__content").removeClass("collection__content--active")
-        $(`.collection__content[data-content-path="${path}"]`).addClass("collection__content--active")
-            // sliderCatalog()
-        if (path == 2) {
-            sliderCatalog2()
-        }
-    })
-    $(".questions__item-show").click(function() {
-        if ($(this).parents(".questions__item").hasClass("questions__item--active")) {
-            $(this).parents(".questions__item").removeClass("questions__item--active")
-        } else {
-            $(".questions__item-show").parents(".questions__item").removeClass("questions__item--active")
-            $(this).parents(".questions__item").addClass("questions__item--active")
-        }
-    })
-
-    $(".catalog__filter-item-show").click(function() {
-        $(this).toggleClass("catalog__filter-item-show--active")
-    })
-
-    $(".catalog__filter-top").click(function() {
-        $(this).toggleClass("catalog__filter-top--active")
-    })
-
-    $(".catalog-raw__item-show").click(function() {
-        if ($(this).hasClass("catalog-raw__item-show--active")) {
-            $(".catalog-raw__item-show").removeClass("catalog-raw__item-show--active")
-        } else {
-            $(".catalog-raw__item-show").removeClass("catalog-raw__item-show--active")
-            $(this).addClass("catalog-raw__item-show--active")
-        }
-
-    })
-
-    $(document).mouseup(function(e) { // событие клика по веб-документу
-        var div = $(".catalog-raw__item-show");
-        if (!div.is(e.target) // если клик был не по нашему блоку
-            &&
-            div.has(e.target).length === 0) { // и не по его дочерним элементам
-            // div.hide()
-            if ($(".catalog-raw__item-show").hasClass("catalog-raw__item-show--active")) {
-                $(".catalog-raw__item-show").removeClass("catalog-raw__item-show--active")
+            if ($(".select-tarif__show").hasClass("select-tarif__show--active")) {
+                $(".select-tarif__show").removeClass("select-tarif__show--active")
             }
         }
     });
+
+    $(".modal__tab").click(function() {
+        let path = $(this).attr("data-tab-path")
+        $(".modal__tab").removeClass("modal__tab--active")
+        $(`.modal__tab[data-tab-path="${path}"]`).addClass("modal__tab--active")
+        $(".modal__form-tab").removeClass("modal__form-tab--active")
+        $(`.modal__form-tab[data-tab-path="${path}"]`).addClass("modal__form-tab--active")
+    })
+
+    $(".information__item-show").click(function() {
+        $(this).parents(".information__right-item").toggleClass("information__right-item--active")
+    })
+
+    $(".certificate__data-close").click(function() {
+        $(".certificate__data").hide()
+    })
+
 })
 
-
-function clickOutsideElemnt(div, e) {
-    if (!div.is(e.target) &&
-        div.has(e.target).length === 0) {
-        div.hide();
-    }
-}
-
-function cartCalc() {
-    $('.ccalc .ccalc-minus').click(function() {
-        let a = $(this).closest('.ccalc').find('input').val();
-        if (a > 1) {
-            let b = +a - 1;
-            $(this).closest('.ccalc').find('input').val(b);
-        } else {
-            $(this).closest('.ccalc').find('input').val(a);
-            $(this).addClass("disabled");
-        }
-    });
-    $('.ccalc .ccalc-plus').click(function() {
-        let a = $(this).closest('.ccalc').find('input').val();
-        let b = +a + 1;
-        $(this).closest('.ccalc').find('input').val(b);
-        $(this).siblings(".ccalc-minus").removeClass("disabled");
-    });
-}
 
 function onIn() {
     if (window.innerWidth > 992) {
@@ -177,23 +138,5 @@ function onIn() {
 function onOut() {
     if (window.innerWidth > 992) {
         $(this).children(".nav__item-show").removeClass("nav__item-show--active")
-    }
-}
-
-function onIn2() {
-    if (window.innerWidth > 992) {
-        let el = $(this)
-        setTimeout(function() {
-            if (el.is(':hover')) {
-                console.log(el)
-                el.children(".header__catalog-item-show").addClass("header__catalog-item-show--active")
-            }
-        }, 200);
-    }
-}
-
-function onOut2() {
-    if (window.innerWidth > 992) {
-        $(this).children(".header__catalog-item-show").removeClass("header__catalog-item-show--active")
     }
 }
